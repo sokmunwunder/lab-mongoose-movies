083d13a4511a887ae6400949363e896e4aedfc8d
Sok Mun Wunder <sokmunwunder@gmail.com>
2021-01-21 13:06:33 +0100

    Iteration 3 completed

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -12,4 +12,24 @@ router.get('/create', (req, res, next) => {
     });
 });
 
+router.get('/create/:id', (req, res, next) => {
+  const id = req.params.id;
+  Celebrity.findById(id)
+    .then((celebrity) => {
+      if (celebrity === null) {
+        const error = new Error('Page does not exist.');
+        error.status = 404;
+        next(error);
+      } else {
+        res.render('celebrities/show', { celebrity: celebrity });
+      }
+    })
+    .catch((error) => {
+      if (error.kind === 'ObjectId') {
+        error.status = 404;
+      }
+      next(error);
+    });
+});
+
 module.exports = router;
