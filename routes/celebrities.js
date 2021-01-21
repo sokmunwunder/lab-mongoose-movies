c1573975ce6031b53b8348f06000d3254bc7308a
Sok Mun Wunder <sokmunwunder@gmail.com>
2021-01-21 15:48:38 +0100

    Iteration 3 again

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -7,10 +7,31 @@ router.get('/create', (req, res, next) => {
     .then((celebrities) => {
       res.render('celebrities/index', { celebrities });
     })
+    /*.then((celebrities) => {
+      res.render('celebrities/show', { celebrities });
+    })*/
     .catch((error) => {
       next(error);
     });
 });
+
+//router.get('/', (req, res, next) => {
+/*router.get('/celebrities', (req, res, next) => {
+  Celebrity.find({})
+    .then((celebrities) => {
+      res.render('celebrities/index', { celebrities });
+    })
+    /*.then((celebrities) => {
+      res.render('celebrities/create', { celebrities });
+    })*/
+/*.catch((error) => {
+      next(error);
+    });
+});*/
+
+/*router.get('/create', (req, res, next) => {
+  res.render('celebrities/create');
+});*/
 
 router.get('/create/:id', (req, res, next) => {
   const id = req.params.id;
